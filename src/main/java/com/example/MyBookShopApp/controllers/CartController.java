92cc8df13bdb6bddf347be5178ce6876eb518394
sashka <iproph@ya.ru>
2022-02-16 12:49:51 +0400

    added all needed pages for module 4 homework

---
+++ src/main/java/com/example/MyBookShopApp/controllers/CartController.java
@@ -0,0 +1,16 @@
+package com.example.MyBookShopApp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CartController {
+
+    public CartController() {
+    }
+
+    @GetMapping("/cart")
+    public String cartPage() {
+        return "/cart";
+    }
+}
